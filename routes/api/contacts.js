bba3f354d9827f1c78643d0e40df2491400cc92f
Oleksandr Chyhryn <as.chigrin@gmail.com>
2023-02-10 03:07:35 +0200

    adding mongoose with code refactoring

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,6 +6,7 @@ const {
   addContactController,
   deleteContactByIdController,
   changeContactByIdController,
+  updateStatusContactController,
 } = require("../../controllers/contactsControllers");
 
 const {
@@ -24,5 +25,7 @@ router.post("/", addContactValidation, addContactController);
 router.delete("/:id", deleteContactByIdController);
 
 router.put("/:id", changeContactByIdValidation, changeContactByIdController);
+
+router.patch("/:id/favorite", updateStatusContactController);
 
 module.exports = { contactsRouter: router };
