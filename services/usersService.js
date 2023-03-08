aa9cf3519b797e9b1a08c96f10b15c6bb9a03b2d
Oleksandr Chyhryn <as.chigrin@gmail.com>
2023-03-08 23:33:08 +0200

    add email verification logic

--- services/usersService.js
+++ services/usersService.js
@@ -4,11 +4,16 @@ const findUser = ({ ...arg }) => {
   return Users.findOne(arg);
 };
 
+const updateUserById = (id, data) => {
+  return Users.findByIdAndUpdate(id, data);
+};
+
 const createUser = ({ ...arg }) => {
   return Users.create(arg);
 };
 
 module.exports = {
   findUser,
+  updateUserById,
   createUser,
 };
